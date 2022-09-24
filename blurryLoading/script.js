3ce13a580bf4de0fb28c53c786240bc04ccdf897
Sean Tolbert <stolbert4150@gmail.com>
2022-09-24 09:46:29 -0400

    completes scroll animation

--- blurryLoading/script.js
+++ blurryLoading/script.js
@@ -3,7 +3,7 @@ const bg = document.querySelector(".bg");
 
 let load = 0;
 
-let int = setInterval(blurring, 30)
+let int = setInterval(blurring, 30);
 
 function blurring() {
   load++;
@@ -12,11 +12,10 @@ function blurring() {
   }
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 40, 0)}px)`
+  bg.style.filter = `blur(${scale(load, 0, 100, 40, 0)}px)`;
 }
-
 
 // function for an incremented scaling process
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
